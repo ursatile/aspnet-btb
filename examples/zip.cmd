e3bffd91e0f196d81cbdd30a160909ef600405f3
Dylan Beattie <dylan@dylanbeattie.net>
2024-08-20 22:38:36 +0100

    ZIP files for all the module downloads

--- examples/zip.cmd
+++ examples/zip.cmd
@@ -2,5 +2,5 @@
 @echo off
 echo Updating ZIP files...
 del *.zip
-for /d %%1 in (*) do 7z a mwnet%%1.zip ./%%1/rockaway/* -mx0 -xr!bin -xr!obj -xr!_NCrunch_Rockaway -xr!.idea -xr!.vs -xr!*.ncrunch*
+for /d %%1 in (*) do 7z a rockaway%%1.zip ./%%1/* -mx0 -xr!bin -xr!obj -xr!_NCrunch_Rockaway -xr!.idea -xr!.vs -xr!*.ncrunch*
 echo All done! Yay!
